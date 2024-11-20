c0c4eff115e3c6bb155e661e992e1e32174f6eaa
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2024-11-20 21:22:09 +0300

    First commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className="bg-slate-600 text-red-700">
+     klashfklasdf
+    </h1>
+  )
+}
+
+export default App
