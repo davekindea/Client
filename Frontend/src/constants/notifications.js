e61566dbd7c15a1a8af4e099a2cc44939bb2f902
abrham111 <abrhambayih733@gmail.com>
2024-11-25 10:28:23 +0300

    Add Notification

---
+++ Frontend/src/constants/notifications.js
@@ -0,0 +1,65 @@
+export const notifications = [
+  {
+      id: 1,
+      profileImg: "/user1.jpg",
+      name: "Mr. Dinber",
+      message: "Your approval is required for the document: Budget_Report.pdf.",
+      time: "2024-11-22T10:00:00Z"
+  },
+  {
+      id: 2,
+      profileImg: "user2.jpg",
+      name: "Ms. Wosen",
+      message: "The document Project_Plan.docx has been updated.",
+      time: "2024-11-22T11:00:00Z"
+  },
+  {
+      id: 3,
+      profileImg: "user3.jpg",
+      name: "Abrham",
+      message: "A new document has been added: Document_01.pdf.",
+      time: "2024-11-22T12:00:00Z"
+  },
+  {
+      id: 4,
+      profileImg: "user4.jpg",
+      name: "Ermias",
+      message: "Reminder: The document Compliance_Guide.pdf is due for review on 2024-12-01.",
+      time: "2024-11-22T13:00:00Z"
+  },
+  {
+      id: 5,
+      profileImg: "user5.jpg",
+      name: "Emily Davis",
+      message: "You have a new version of the document Research_Study.pdf.",
+      time: "2024-11-22T14:00:00Z"
+  },
+  {
+      id: 6,
+      profileImg: "user6.jpg",
+      name: "Frank Wilson",
+      message: "John Doe has shared the document Team_Meeting_Agenda.docx with you.",
+      time: "2024-11-22T15:00:00Z"
+  },
+  {
+    id: 7,
+    profileImg: "user7.jpg",
+    name: "Frank Wilson",
+    message: "John Doe has shared the document Team_Meeting_Agenda.docx with you.",
+    time: "2024-11-22T15:00:00Z"
+},
+{
+    id: 8,
+    profileImg: "user8.jpg",
+    name: "Frank Wilson",
+    message: "John Doe has shared the document Team_Meeting_Agenda.docx with you.",
+    time: "2024-11-22T15:00:00Z"
+},
+{
+    id: 9,
+    profileImg: "user9.jpg",
+    name: "Frank Wilson",
+    message: "John Doe has shared the document Team_Meeting_Agenda.docx with you.",
+    time: "2024-11-22T15:00:00Z"
+}
+];
